7ed3e6c138ebb35b31c1b4fc802635b8603e363f
dung <danghoangdung2004@gmail.com>
2023-11-14 11:02:41 +0100

    my first commit

---
+++ register.php
@@ -0,0 +1,51 @@
+<?php
+
+$username = $_POST['username'];
+$email  = $_POST['email'];
+$password = $_POST['password'];
+
+if (!empty($username) || !empty($email) || !empty($password))
+{
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "combined";
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $SELECT = "SELECT email From register Where email = ? Limit 1";
+  $INSERT = "INSERT Into register (username, email, password )values(?,?,?)";
+
+//Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+
+     //checking username
+      if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("sss", $username,$email,$password);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
